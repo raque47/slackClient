67f2c6daf6eb86072e837544b7c6fb0415aa36e0
raque47 <raquelchava_94@hotmail.com>
2017-06-26 10:36:42 -0600

    Correction in url of client

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,9 @@ import store from '../store';
 import Cookies from 'universal-cookie';
 import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, SET_USER, SET_ALL_USERS, SET_FETCH_READY, SET_CHAT, SET_MESSAGE, SET_USER_LOGGED, SET_MESSAGES_FOR_EVERYONE, SET_CURRENT_MESSAGES, SET_MESSAGES_TYPE, UPDATE_MESSAGE, UPDATE_MESSAGE_CHANNELS } from './types';
 
-const API_URL = 'https://agile-journey-45148.herokuapp.com:3000/api';
-const API_URL_ROUTES = 'https://agile-journey-45148.herokuapp.com:3000/api/routes';
-
+const API_URL = 'https://agile-journey-45148.herokuapp.com/api';
+const API_URL_ROUTES = 'https://agile-journey-45148.herokuapp.com/api/routes';
+const API_CLIENTE= 'https://grader-toad-67805.netlify.com/';
 // const API_URL = 'http://localhost:3000/api';
 // const API_URL_ROUTES = 'http://localhost:3000/api/routes';
 
@@ -26,7 +26,7 @@ export function loginUser({ email, password }) {
                 dispatch({ type: AUTH_USER });
                 const cookies = new Cookies();
                 cookies.set('token', response.data.user, { path: '/' });
-                window.location.href = '/chat';
+                window.location.href = API_CLIENTE+ '/chat';
             })
             .catch((error) => {
                 console.log('Axios error');
@@ -45,7 +45,7 @@ export function registerUser({ firstName, lastName, email, password }) {
                 dispatch({ type: AUTH_USER });
                 const cookies = new Cookies();
                 cookies.set('token', response.data.user, { path: '/' });
-                window.location.href = '/chat';
+                window.location.href =  API_CLIENTE+'/chat';
             })
             .catch((error) => {
                 // errorHandler(dispatch, error.response, AUTH_ERROR)
